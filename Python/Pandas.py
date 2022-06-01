548aff55c719de48cadf04ec76f5593a454ef741
Andrew55529 <lashkov8@gmail.com>
2022-06-01 11:07:35 +0300

    1

--- Python/Pandas.py
+++ Python/Pandas.py
@@ -20,6 +20,40 @@ bp = sns.boxplot(x = "variable", y = 'value', data = pd.melt(dff), whis = [0, 10
 
 plt.show()
 
+
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import matplotlib.pyplot as mpl
+from mpl_toolkits.mplot3d import axes3d
+
+fig = mpl.figure()
+ax = fig.add_subplot(111, projection = '3d')
+
+i = np.arange(-1,1,0.01)
+X, Y = np.meshgrid(i,i)
+Z = X**2-Y**2
+
+ax.plot_wireframe(X,Y,Z,rstride = 4, cstride = 4)
+
+x = y = np.linspace(-3,3,74)
+X, Y = np.meshgrid(x,y)
+
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(4 * R) / R
+
+fig, ax = mpl.subplots(1,3,figsize = (14,4), subplot_kw=dict(projection = '3d'))
+
+p = ax[0].plot_surface(X, Y, Z, rstride = 1,cstride = 1)
+ax[0].set_xlabel("$x$",fontsize = 16)
+ax[0].set_xlabel("$y$",fontsize = 16)
+ax[0].set_xlabel("$z$",fontsize = 16)
+
+ax[1].plot_wireframe(X,Y,Z, rstride = 3, cstride = 3, color = "darkgrey")
+ax[1].set_title("plot_wireframe")
+
+
+
 import numpy as np
 import pandas as pd
 import matplotlib as plt
