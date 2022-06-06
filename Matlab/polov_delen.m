5b8d1710e614df8ae2412e23fc30393c539ec76f
Andrew55529 <lashkov8@gmail.com>
2022-06-06 10:37:57 +0300

    Update polov_delen.m

--- Matlab/polov_delen.m
+++ Matlab/polov_delen.m
@@ -1,8 +1,6 @@
-clear
 a = 0;
 b = 4*pi;
 x = linspace(a,b,100)
-h = 0.001;
 maxcount=100;
 eps=0.001;
 f=@(x)sin(x).*(x)-cos(x);
@@ -10,15 +8,16 @@ plot(x,f(x),'.',x,0*x,'-'); grid on
 xlabel('x'); ylabel('y')
 hold on
 z = ginput(2); 
-z1=z(1,1), z2=z(2,1);
-f1 = f(z1); f2=f(z2); z=(z1+z2)/2; y=f(z);
+z1=z(1,1), z2=z(2,1); z=(z2-z1)/2+z1;
+f1 = f(z1); f2=f(z2); y=f(z);
 P = plot(z1,0,'*',z2,0,'*',z,0,'o');
 if f1*f2>0 
     'Error'
 end
 i=0;
 while(i<maxcount)
-    z=(z1+z2)/2; y=f(z);
+    z=(z2-z1)/2+z1;
+    y=f(z);
     P=plot(z1,0,'*',z2,0,'*',z,0,'o');
     if abs(f(z))<eps
         break;
